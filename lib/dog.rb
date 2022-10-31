3cc7487e95e12e2b689f66d745252d01d4236979
Chris Li <chris.lty07@gmail.com>
2022-10-31 15:57:58 -0400

    commit tree 17616e6d8e97fbb79cb84cef1ba12c54e5f7333e

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+    # setter method
+    def name=(dogs_name)
+        # assign the @name instance variable a value of whatever is passed as an argument
+        @this_dogs_name = dogs_name
+    end
 
+    def name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+
+lassie.name = "Lassie"
+
+puts lassie.name
